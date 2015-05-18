fb8fb96d634a5149fb35c1a7b7085f9d2f34f92f
shgfl007 <shgfl@hotmail.com>
2015-05-17 23:27:49 -0400

    portfolio site

---
+++ webGL_test/Assets/Scripts/onCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onCollision : MonoBehaviour {
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Debug.Log("Enter Called");
+	}
+
+	void OnCollisionStay(Collision collision)
+	{
+		Debug.Log ("Stay occuring...");
+	}
+
+	void OnCollisionExit (Collision collision)
+	{
+		Debug.Log("Exit Called..");
+	}
+}
